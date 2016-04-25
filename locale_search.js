defe766ecb2584335ca2735023b072db7e885e88
David Silva <zgrav@go.su>
2016-04-25 23:44:22 +0100

    removal of console.log on file save

--- locale_search.js
+++ locale_search.js
@@ -101,6 +101,4 @@ fs.writeFile(process.argv[2], JSON.stringify(result, null, 2), function(err) {
     if(err) {
         return console.log(err);
     }
-
-    console.log("The file was saved!");
 });
